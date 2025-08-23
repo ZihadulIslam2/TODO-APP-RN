5bf05acbfcaf46b8c14ffce3fe82e8327d302ad4
zihadulislam2 <zihadul708@gmail.com>
2025-08-23 06:24:27 +0600

    feat(dynamicColors)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,40 @@
+import { api } from '@/convex/_generated/api'
+import { useMutation, useQuery } from 'convex/react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import useTheme from '../hooks/useTheme'
+import useTheme, { ColorScheme } from '../hooks/useTheme'
 
-export default function Tab() {
-  const { toggleDarkMode } = useTheme()
+export default function Index() {
+  const todos = useQuery(api.todo.getTodos)
+
+  const addTodo = useMutation(api.todo.addTodo)
+
+  console.log(1, todos)
+
+  const { toggleDarkMode, colors } = useTheme()
+
+  const styles = createStyles(colors)
   return (
     <View style={styles.container}>
-      <Text>Tab [Home|Settings] dfsdf</Text>
+      <Text>Tab [Home|Settings]</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>toggle the mode</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => addTodo({ text: 'Do the home work!' })}>
+        <Text>Add a new project</Text>
       </TouchableOpacity>
     </View>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-})
+const createStyles = (colors: ColorScheme) => {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: colors.bg,
+    },
+  })
+  return styles
+}
